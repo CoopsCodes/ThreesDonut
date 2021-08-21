63b1d2b8e2209fb0ef86ca9e9ab83cccd02433f5
Cooper Viktor <cooper@gracepapers.com.au>
2021-08-21 19:33:56 +1000

    adding publishing file

---
+++ dist/src/World/World.js
@@ -0,0 +1,55 @@
+import { loadDonuts } from "./components/donuts/donut";
+import { createScene } from "./components/scene";
+import { createLights } from "./components/lights";
+import { createCamera } from "./components/camera";
+
+import { Loop } from "./systems/Loop";
+import { Resizer } from "./systems/Resizer";
+import { createControls } from "./systems/controls";
+import { createRenderer } from "./systems/renderer";
+
+let camera;
+let controls;
+let renderer;
+let scene;
+let loop;
+
+class World {
+  constructor(container) {
+    camera = createCamera();
+    renderer = createRenderer();
+    scene = createScene();
+    loop = new Loop(camera, scene, renderer);
+    container.append(renderer.domElement);
+    controls = createControls(camera, renderer.domElement);
+
+    const { ambientLight, mainLight } = createLights();
+
+    loop.updatables.push(controls);
+    scene.add(ambientLight, mainLight);
+
+    const resizer = new Resizer(container, camera, renderer);
+  }
+  async init() {
+    const { donut } = await loadDonuts();
+    console.log({ donut });
+
+    controls.target.copy(donut.position);
+
+    scene.add(donut);
+  }
+
+  render() {
+    renderer.render(scene, camera);
+  }
+
+  start() {
+    loop.start();
+  }
+
+  stop() {
+    loop.stop();
+  }
+}
+
+export { World };
---
+++ dist/src/script.js
@@ -0,0 +1,16 @@
+import "./style.css";
+import { World } from "./World/World";
+
+async function main() {
+  const container = document.querySelector("#scene-container");
+
+  const world = new World(container);
+
+  await world.init();
+
+  world.start();
+}
+
+main().catch((error) => {
+  console.error(error);
+});
